0c80589b34dc81f8c2c254fc2f75ba95be9434b1
Paul Klink <paul@klink.au>
2024-12-09 14:39:48 +1100

    Add content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+const canonicalHost = 'klink.au';
+
+export const githubHost = 'pbklink.github.io'; // Defined in Astro Config. In future get this from environmental variable
+export const githubSite = `https://${githubHost}`;
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: githubSite,
+    trailingSlash: 'always',
+
+    integrations: [
+        sitemap({
+            // Change sitemap URLs to use custom host supplied to GitHub.
+            serialize(item) {
+                const url = new URL(item.url);
+                if (url.host === githubHost) {
+                    url.host = canonicalHost;
+                }
+                item.url = url.href;
+                return item;
+            }
+        })
+    ],
+});
